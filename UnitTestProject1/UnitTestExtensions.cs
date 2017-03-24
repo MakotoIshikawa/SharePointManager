9e63426cfef7bc5bab8fab90030392d368877cc8
makoto_win8 <ishikawm@fsi.co.jp>
2017-03-24 15:40:46 +0900

    ライブラリー外出し

--- UnitTestProject1/UnitTestExtensions.cs
+++ UnitTestProject1/UnitTestExtensions.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using CommonFeaturesLibrary.Extensions;
 using ExtensionsLibrary.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
